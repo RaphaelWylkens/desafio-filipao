5b40162d29ebbbe45f3f4ef8c07ef545da100ae2
RaphaelWylkens <raphaelt2410@gmail.com>
2025-12-31 11:12:42 -0300

    Alterações do desafio-filipao

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log("Olá mundo!")
+let nome = "Raphael"
+let xp = 600
+let nivel = ""
+
+
+if(xp >= 10001){
+    nivel = "Radiante"
+}else if(xp >= 9001){
+    nivel = "Imortal"
+}else if (xp >= 8001){
+    nivel = "Ascedente"
+}else if(xp >= 7001){
+    nivel = "Planita"
+}else if(xp >= 5001){
+    nivel = "Ouro"    
+}else if(xp >= 2001){
+    nivel = "Prata"
+}else if(xp >= 1001){
+    nivel = "Bronze"
+}else{
+    nivel = "Ferro"
+}
+
+console.log("O Herói de nome " + nome + " está no nível " +  nivel)
+
+
